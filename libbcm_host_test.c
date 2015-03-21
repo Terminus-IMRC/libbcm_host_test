9cfec054dabc55bfda7b35074a9b835c446f8bdc
Terminus <i.can.speak.c.and.basic@gmail.com>
2015-03-21 19:09:15 +0900

    initial commit

---
+++ libbcm_host_test.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <inttypes.h>
+#include "bcm_host.h"
+
+int main()
+{
+	int addr_width = sizeof(int) * 2;
+
+	bcm_host_init();
+
+	printf("Peripheral address: 0x%0*x\n", addr_width, bcm_host_get_peripheral_address());
+	printf("Peripheral size: %d\n", bcm_host_get_peripheral_size());
+	printf("SDRAM address: 0x%0*x\n", addr_width, bcm_host_get_sdram_address());
+
+	bcm_host_deinit();
+
+	return 0;
+}
