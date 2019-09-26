6a2b7ded46ff11e4a0287ddce508f0c236feb2df
Sugizaki Yukimasa <ysugi@idein.jp>
2019-09-26 13:45:42 +0900

    Support for the new queries

--- libbcm_host_test.c
+++ libbcm_host_test.c
@@ -5,9 +5,53 @@ int main(void)
 {
     bcm_host_init();
 
-    printf("Peripheral address: %#010x\n", bcm_host_get_peripheral_address());
-    printf("Peripheral size: %#010x\n", bcm_host_get_peripheral_size());
-    printf("SDRAM address: %#010x\n", bcm_host_get_sdram_address());
+    printf("peripheral_address: %#010x\n", bcm_host_get_peripheral_address());
+    printf("peripheral_size: %#010x\n", bcm_host_get_peripheral_size());
+    printf("sdram_address: %#010x\n", bcm_host_get_sdram_address());
+
+    const unsigned model_type = bcm_host_get_model_type();
+    switch (model_type) {
+#define X(x) case x: printf("model_type: %u (%s)\n", x, #x); break;
+        X(BCM_HOST_BOARD_TYPE_MODELA)
+        X(BCM_HOST_BOARD_TYPE_MODELB)
+        X(BCM_HOST_BOARD_TYPE_MODELAPLUS)
+        X(BCM_HOST_BOARD_TYPE_MODELBPLUS)
+        X(BCM_HOST_BOARD_TYPE_PI2MODELB)
+        X(BCM_HOST_BOARD_TYPE_ALPHA)
+        X(BCM_HOST_BOARD_TYPE_CM)
+        X(BCM_HOST_BOARD_TYPE_CM2)
+        X(BCM_HOST_BOARD_TYPE_PI3MODELB)
+        X(BCM_HOST_BOARD_TYPE_PI0)
+        X(BCM_HOST_BOARD_TYPE_CM3)
+        X(BCM_HOST_BOARD_TYPE_CUSTOM)
+        X(BCM_HOST_BOARD_TYPE_PI0W)
+        X(BCM_HOST_BOARD_TYPE_PI3MODELBPLUS)
+        X(BCM_HOST_BOARD_TYPE_PI3MODELAPLUS)
+        X(BCM_HOST_BOARD_TYPE_FPGA)
+        X(BCM_HOST_BOARD_TYPE_CM3PLUS)
+        X(BCM_HOST_BOARD_TYPE_PI4MODELB)
+#undef X
+        default:
+            printf("model_type: %u (Unknown)\n", model_type);
+            break;
+    }
+
+    printf("is_model_pi4: %u\n", bcm_host_is_model_pi4());
+    printf("is_fkms_active: %u\n", bcm_host_is_fkms_active());
+    printf("is_kms_active: %u\n", bcm_host_is_kms_active());
+
+    const unsigned processor_id = bcm_host_get_processor_id();
+    switch (processor_id) {
+#define X(x) case x: printf("processor_id: %u (%s)\n", x, #x); break;
+        X(BCM_HOST_PROCESSOR_BCM2835)
+        X(BCM_HOST_PROCESSOR_BCM2836)
+        X(BCM_HOST_PROCESSOR_BCM2837)
+        X(BCM_HOST_PROCESSOR_BCM2838)
+#undef X
+        default:
+            printf("processor_id: %u (Unknown)\n", processor_id);
+            break;
+    }
 
     bcm_host_deinit();
 
